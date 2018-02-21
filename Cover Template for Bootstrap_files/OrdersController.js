1cc3406e4c03457aa5f0c6003c7574d1cadf243f
sebastianreyes01@hotmail.com <2117816@labinfo.is.escuelaing.edu.co>
2018-02-21 13:36:40 -0500

    adelanto funcion addOrder

--- Cover Template for Bootstrap_files/OrdersController.js
+++ Cover Template for Bootstrap_files/OrdersController.js
@@ -1,5 +1,17 @@
-var order = {orderId:1,orderItems:[{Product:"PIZZA",Quantity:3,Price:10000},{Product:"HOTDOG",Quantity:1,Price:3000},{Product:"COKE",Quantity:4,Price:1300}]};
+var orders = [{orderId:0,orderItems:[{Product:"PIZZA",Quantity:3,Price:10000},{Product:"HOTDOG",Quantity:1,Price:3000},{Product:"COKE",Quantity:4,Price:1300}]}];
 
-function addOrder(prod, q, pri) {
-    order.orderItems.push({Product:prod,Quantity:q,Price:pri});
+function addOrderItem(id,prod, q, pri) {
+    orders[id].orderItems.push({Product:prod,Quantity:q,Price:pri});
+}
+
+function addOrder(id,orderItems) {
+	if (orders.length >= id) {
+		orders.push({orderId:id,orderItems:orderItems})
+	}
+}
+
+function removeOrderById(int id) {
+	if (id >= orders.length) {
+		
+	}
 }
